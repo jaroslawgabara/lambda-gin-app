5f066e07a084e9c8a7b471965fd8bb223c56395e
Jarek <jaroslawgabara@outlook.com>
2020-12-11 14:55:37 +0100

    Added Middleware

--- api.go
+++ api.go
@@ -5,3 +5,19 @@ import "github.com/gin-gonic/gin"
 func StartApi(r *gin.Engine) {
 	r.Run()
 }
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
